ac686b51b7be2e69067c97a1a40e39db2d3109bd
bobsoros <bobsoros0@gmail.com>
2019-10-08 04:16:26 +0700

    tambah project keempat

--- MyviewandViews/app/src/main/java/com/example/myviewandviews/MainActivity.java
+++ MyviewandViews/app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.myviewandviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnDonasi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (getSupportActionBar()!=null){
-            getSupportActionBar().setTitle("Google Pixel");
+        btnDonasi = findViewById(R.id.btndonasi);
+        btnDonasi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btndonasi:
+                Intent movedonasi = new Intent(MainActivity.this, donasi.class);
+                startActivity(movedonasi);
         }
     }
 }
