9fa5188de8ef981757e08b0bc26a5a461ba34d78
bobsoros <bobsoros0@gmail.com>
2019-11-12 15:44:39 +0700

    UTS Mobile Programming

---
+++ hmjpolines/app/src/main/java/bob/example/hmjpolines/cardhmjadapter.java
@@ -0,0 +1,14 @@
+package bob.example.hmjpolines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardhmjadapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardhmjadapter);
+    }
+}
